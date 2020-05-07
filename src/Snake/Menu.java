0e11a1ddbee8680bc6915eb84e9af7c4e4ef17a0
Wojciech Jankowski <wjankowski96@wp.pl>
2020-05-07 20:05:03 +0200

    konstruktorze klasy tworzymy nowe okno (obiekt) o nazwie menu. Ustalamy parametry, w tym rozmiar okna (proszę się zapoznać z pozostałymi metodami). Następnie tworzymy panel jednokolumnowy metodą GridLayout.

--- src/Snake/Menu.java
+++ src/Snake/Menu.java
@@ -34,11 +34,19 @@ public class Menu extends JFrame {
 	 * Create the frame.
 	 */
 	public Menu() {
+		JFrame menu = new JFrame();
+		menu.setUndecorated(true);
+		menu.setFocusable(true);
+		menu.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		menu.setResizable(false);
+		menu.pack();
+		menu.setLocationRelativeTo(null);
+		menu.setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
+		contentPane.setLayout(new GridLayout(0,1,0, 0));
 		setContentPane(contentPane);
 	}
 
